7a8b1d0f446c716cf35db6a457dff99ebe5f8685
Nam Tang <nam.tang@live.fr>
2022-09-28 14:14:35 +0200

    layers prime

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {},
+    colors: {
+      'Very-Dark-Grayish-Blue': 'hsl(217, 19%, 35%)',
+      'Desaturated-Dark-Blue': 'hsl(214, 17%, 51%)',
+      'Grayish-Blue': 'hsl(212, 23%, 69%)',
+      'Light-Grayish-Blue': 'hsl(210, 46%, 95%)'
+    }
   },
   plugins: [],
 }
